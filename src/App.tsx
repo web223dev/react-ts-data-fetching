3e60ea33c4b62042bd7f6f5e7775e653b8e5c9c7
web223dev <ruslanus866@outlook.com>
2024-08-21 23:19:10 +0800

    add built-in get function and display posts from dummy API

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,50 @@
+import { ReactNode, useEffect, useState } from 'react';
+
+import { get } from './util/http';
+import BlogPosts, { BlogPost } from './components/BlogPosts';
+import fetchingImg from './assets/data-fetching.png';
+
+type RowDataBlogPost = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
+
 function App() {
-  return <h1>Data Fetching!</h1>;
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[] | undefined>();
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const data = (await get('https://jsonplaceholder.typicode.com/posts')) as RowDataBlogPost[];
+
+      const blogPosts: BlogPost[] = data.map((rowPost) => {
+        return {
+          id: rowPost.id,
+          userId: rowPost.userId,
+          title: rowPost.title,
+          text: rowPost.body,
+        };
+      });
+
+      setFetchedPosts(blogPosts);
+    }
+
+    fetchPosts();
+  }, []);
+
+  let content: ReactNode;
+
+  if (fetchedPosts) {
+    content = <BlogPosts posts={fetchedPosts} />;
+  }
+
+  return (
+    <main>
+      <img src={fetchingImg} alt='An abstract image depicting a data fetching process.' />
+      {content}
+    </main>
+  );
 }
 
 export default App;
